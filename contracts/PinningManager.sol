aa09c905f2b0635fc25c668b73a1b1c297576903
eknir <rinkehendriksen@gmail.com>
2020-01-21 16:40:34 +0100

    verify all math operations on over/underflow

--- contracts/PinningManager.sol
+++ contracts/PinningManager.sol
@@ -7,10 +7,13 @@ import "./vendor/SafeMath.sol";
 /// @notice Storage providers can offer their storage space and list their price and clients can take these offers
 contract PinningManager {
 
-    //**TODO: verify all math operations and use SafeMath where needed.
     //**TODO: make a seperate contract per request?
-    // using SafeMath for uint256;
-    // using SafeMath for uint128;
+    //**TODO: make helper function for isRequestExpired
+    //**TODO: verify that all safeMath operations don't cause a deadlock in the contract
+    //**TODO: re-verify all math operations
+    using SafeMath for uint256;
+    using SafeMath for uint128;
+    using SafeMath for uint64;
     uint64 constant MAX_UINT64 = 18446744073709551615;
 
     /*
@@ -70,6 +73,11 @@ contract PinningManager {
     @notice set the capacity, maximumDuration and price of a StorageOffer.
     @dev use this function when initiating a storage offer or when the users wants to change more than one parameter at once. TODO: gas price comparison.
     maximumDuration must be smaller or equal to the longest period (NOT verified by smart-contract).
+    Exercise caution with assigning additional capacity when capacity is already taken.
+    It may happen that when a lot of capcity is available and we release already-taken capacity, capacity overflows.
+    We explicitely allow this overflow to happen on the smart-contract level,
+    because the worst thing that can happen is that the provider now has less storage available than planned (in which case he can top it up himself).
+    However, take care of this in the client. REF_CAPACITY
     @param capacity the amount of bytes offered.
     If already active before and set to 0, existing contracts can't be prolonged / re-started, no new contracts can be started.
     @param maximumDuration the maximum time (in seconds) for which a proposer can prepay. Prepaid bids can't be cancelled REF1.
@@ -88,6 +96,11 @@ contract PinningManager {
     /**
     @notice set the capacity of a StorageOffer.
     If already active before and set to 0, existing contracts can't be prolonged / re-started, no new contracts can be started.
+    @dev exercise caution with assigning additional capacity when capacity is already taken.
+    It may happen that when a lot of capcity is available and we release already-taken capacity, capacity overflows.
+    We explicitely allow this overflow to happen on the smart-contract level,
+    because the worst thing that can happen is that the provider now has less storage available than planned (in which case he can top it up himself).
+    However, take care of this in the client. REF_CAPACITY
     @param capacity the amount of bytes offered.
     */
     function setStorageCapacity(uint128 capacity) public {
@@ -117,6 +130,7 @@ contract PinningManager {
         _setMaximumDuration(offer, maximumDuration);
     }
 
+
     /**
     @notice request to fill a storageOffer. After requesting, an offer must be accepted by provider to become active.
     @dev if Request was active before, is expired and final payout is not yet done, final payout can be triggered by proposer here.
@@ -127,35 +141,47 @@ contract PinningManager {
     @param period the chosen period (seconds after which a Request can be cancelled and left-over money refunded).
     */
     function newRequest(bytes32 fileReference, address payable provider, uint128 size, uint64 period) public payable {
+        require(period != 0, "PinningManager: period of 0 not allowed");
         bytes32 requestReference = getRequestReference(msg.sender, fileReference);
         uint64 chosenPrice = offerRegistry[provider].prices[period];
         require(chosenPrice != 0, "PinningManager: price doesn't exist for provider");
         require(msg.value != 0 && msg.value % chosenPrice == 0, "PinningManager: value sent not corresponding to price");
         StorageOffer storage offer = offerRegistry[provider];
         Request storage request = offer.RequestRegistry[fileReference];
+        //TODO: what if we are so much past the start date that this one always reverts?
+        bool isPastCurrentEndTime = request.startDate.add(request.numberOfPeriodsDeposited.mul(request.chosenPeriod)) < now;
         require(
             request.startDate == 0 ||
-            request.startDate + (request.numberOfPeriodsDeposited * request.chosenPeriod) > now,
+            isPastCurrentEndTime,
             "PinningManager: Request already active"
         );
-        if(request.startDate + (request.numberOfPeriodsDeposited * request.chosenPeriod) > now) {
+        if(isPastCurrentEndTime) {
             require(offer.capacity != 0, "PinningManager: provider discontinued service");
-            uint256 toTransfer = (request.numberOfPeriodsDeposited - request.numberOfPeriodsWithdrawn) * request.chosenPrice;
+            //NO_OVERFLOW reasoning: numberOfPeriodsDeposited always bigger or equal to numberOfPeriodsWithdrawn
+            uint256 toTransfer = (request.numberOfPeriodsDeposited - request.numberOfPeriodsWithdrawn).mul(request.chosenPrice);
             request.numberOfPeriodsWithdrawn = 0;
             request.startDate = 0;
-            offerRegistry[msg.sender].capacity = offerRegistry[msg.sender].capacity + request.size;
-            provider.transfer(toTransfer);
+            /*
+            ALLOWING_OVERFLOW reasoning: the worst that can happen is that the provider has less storage on offer than planned.
+            Verifying during all contract calls for no overflow is too costly to prevent such a small damage.
+            Damage can be easily re-done by calling setStorageCapacity. REF_CAPACITY
+            */
+            offerRegistry[msg.sender].capacity += request.size;
+            provider.transfer(toTransfer); //TODO: transfer is not best practice: https://diligence.consensys.net/blog/2019/09/stop-using-soliditys-transfer-now/
             emit EarningsWithdrawn(requestReference);
         } else {
             request.size = size;
         }
+        //NO_OVERFLOW reasoning: chosenPrice is verified to not be zero in this function call
         uint256 numberOfPeriodsDeposited = msg.value / chosenPrice;
         require(
-            numberOfPeriodsDeposited * period <= offer.maximumDuration ||
+            numberOfPeriodsDeposited.mul(period) <= offer.maximumDuration &&
             numberOfPeriodsDeposited <= MAX_UINT64,
             "PinningManager: period too long"
         );
+        numberOfPeriodsDeposited.mul(chosenPrice); // overFlow check. If this doesn't pass, we might have issues transfering the maximum amount REF_MAX_TRANSFER
         request.chosenPrice = chosenPrice;
+        request.chosenPeriod = period;
         request.numberOfPeriodsDeposited = uint64(numberOfPeriodsDeposited);
         emit RequestMade(
             fileReference,
@@ -175,6 +201,7 @@ contract PinningManager {
     function stopRequestBefore(bytes32 fileReference, address provider) public {
         bytes32 requestReference = getRequestReference(msg.sender, fileReference);
         Request storage request = offerRegistry[provider].RequestRegistry[requestReference];
+        //NO_OVERFLOW reasoning: we already verified: REF_MAX_TRANSFER
         uint256 toTransfer = request.numberOfPeriodsDeposited * request.chosenPrice;
         request.numberOfPeriodsDeposited = 0;
         msg.sender.transfer(toTransfer);
@@ -191,7 +218,8 @@ contract PinningManager {
         Request storage request = offerRegistry[msg.sender].RequestRegistry[requestReference];
         require(request.numberOfPeriodsDeposited != 0);
         request.startDate = uint128(now);
-        offerRegistry[msg.sender].capacity = offerRegistry[msg.sender].capacity - request.size;
+        //TODO: strange behavior below. Why do we need to cast to uint128. If not, compiler complains
+        offerRegistry[msg.sender].capacity = uint128(offerRegistry[msg.sender].capacity.sub(request.size));
         emit RequestAccepted(requestReference);
     }
 
@@ -209,18 +237,22 @@ contract PinningManager {
         require(request.startDate != 0, "PinningManager: Request not active");
         require(offer.prices[request.chosenPeriod] != 0, "PinningManager: price not available anymore");
         require(msg.value != 0 && msg.value % request.chosenPrice == 0, "PinningManager: value sent not corresponding to price");
-        require(request.startDate + (request.numberOfPeriodsDeposited * request.chosenPeriod) <= now, "PinningManager: Request expired");
-        uint64 numberOfPeriods = uint64(msg.value / request.chosenPrice);
+        require(request.startDate.add(request.numberOfPeriodsDeposited.mul(request.chosenPeriod)) <= now, "PinningManager: Request expired");
+        //NO_OVERFLOW reasoning: chosenPrice is verified to not be zero in function call: newRequest
+        uint256 numberOfPeriodsDeposited = msg.value / request.chosenPrice;
         // periodsPast = (now - request.startDate) /  request.chosenPeriod
         // periodsLeft = request.numberOfPeriodsDeposited - periodsPast;
         require(
             (
-                request.numberOfPeriodsDeposited -
-                (now - request.startDate) / request.chosenPeriod +
-                numberOfPeriods
-            ) * request.chosenPeriod <= offer.maximumDuration,
+                //NO_OVERFLOW reasoning: verified in "Request expired" require above
+                request.numberOfPeriodsDeposited - (now - request.startDate) / request.chosenPeriod
+                .add(numberOfPeriodsDeposited)
+            ).mul(request.chosenPeriod) <= offer.maximumDuration &&
+            numberOfPeriodsDeposited.add(request.numberOfPeriodsDeposited) <= MAX_UINT64,
             "PinningManager: period too long");
-        request.numberOfPeriodsDeposited += numberOfPeriods;
+        numberOfPeriodsDeposited.mul(request.chosenPrice); // overFlow check. If this doesn't pass, we might have issues transfering the maximum amount REF_MAX_TRANSFER
+        // NO_OVERFLOW reasoning: verified in line above
+        request.numberOfPeriodsDeposited += uint64(numberOfPeriodsDeposited);
         emit RequestTopUp(requestReference, msg.value);
     }
 
@@ -232,13 +264,16 @@ contract PinningManager {
     function stopRequestDuring(bytes32 fileReference, address provider) public payable {
         bytes32 requestReference = getRequestReference(msg.sender, fileReference);
         Request storage request = offerRegistry[provider].RequestRegistry[requestReference];
-        uint periodsPast = (now - request.startDate) /  request.chosenPeriod + 1;
-        uint periodsLeft = request.numberOfPeriodsDeposited - periodsPast;
+        // NO_OVERFLOW reasoning: startDate is always less than now. request.chosenPeriod is verified not to be 0 in function: newRequest
+        uint256 periodsPast = ((now - request.startDate) / request.chosenPeriod) + 1;
+        require(request.numberOfPeriodsWithdrawn + periodsPast < request.numberOfPeriodsDeposited, "PinningManager: request expired or in last period");
         request.numberOfPeriodsDeposited = 0;
         request.numberOfPeriodsWithdrawn = 0;
+        //NO_OVERFLOW reasoning: REF_CAPACITY
         offerRegistry[msg.sender].capacity = offerRegistry[msg.sender].capacity + request.size;
         request.startDate = 0;
-        msg.sender.transfer(periodsLeft * request.chosenPrice);
+        //NO_OVERFLOW reasoning:  verified in "request expired" require and see REF_MAX_TRANSFER
+        msg.sender.transfer(request.numberOfPeriodsDeposited - periodsPast * request.chosenPrice);
         emit RequestStopped(requestReference);
     }
 
@@ -247,21 +282,30 @@ contract PinningManager {
     @param requestReferences reference to one or more Requests
     */
     function withdrawEarnings(bytes32[] memory requestReferences) public {
-        uint toTransfer;
+        uint256 toTransfer;
         for(uint8 i = 0; i <= requestReferences.length; i++) {
             Request storage request = offerRegistry[msg.sender].RequestRegistry[requestReferences[i]];
             require(request.startDate != 0, "PinningManager: Request not active");
-            //TODO: casting to 128 doesn't work if now is too far away from the startDate
-            uint64 periodsPast = uint64((now - request.startDate) /  request.chosenPeriod);
-            request.numberOfPeriodsWithdrawn += periodsPast;
-            if(request.numberOfPeriodsWithdrawn + periodsPast >= request.numberOfPeriodsDeposited && offerRegistry[msg.sender].capacity != 0) {
-                toTransfer += request.numberOfPeriodsDeposited - request.numberOfPeriodsWithdrawn;
+            // request expired
+            //TODO: what if the safemath operators revert here. Is capital locked-up?
+            if(request.startDate.add(request.numberOfPeriodsDeposited.mul(request.chosenPeriod)) < now) {
+                //NO_OVERFLOW reasoning: numberOfPeriodsWithdrawn is always less than or equal to numberOfPeriodsDeposited and REF_MAX_TRANSFER
+                toTransfer = toTransfer.add((request.numberOfPeriodsDeposited - request.numberOfPeriodsWithdrawn) * request.chosenPrice);
                 request.numberOfPeriodsWithdrawn = 0;
                 request.numberOfPeriodsDeposited = 0;
-                offerRegistry[msg.sender].capacity = offerRegistry[msg.sender].capacity + request.size;
+                if(offerRegistry[msg.sender].capacity != 0) {
+                    //ALLOW_OVERFLOW reasoning: see: REF_CAPACITY
+                    offerRegistry[msg.sender].capacity = offerRegistry[msg.sender].capacity + request.size;
+                }
                 request.startDate = 0;
             } else {
-                toTransfer += periodsPast - request.numberOfPeriodsWithdrawn;
+                // NO_OVERFLOW reasoning: startDate is always less than now. request.chosenPeriod is verified not to be 0 in function: newRequest
+                uint256 periodsPast = (now - request.startDate) /  request.chosenPeriod;
+                ////NO_OVERFLOW reasoning: numberOfPeriodsWithdrawn is always less than or equal to periodsPast and REF_MAX_TRANSFER
+                toTransfer = toTransfer.add((periodsPast - request.numberOfPeriodsWithdrawn) * request.chosenPrice);
+                //NO_OVERFLOW reasoning: request is not expired. TODO: better argumentation here
+                //TODO: is explicit cast possible/safe here?
+                request.numberOfPeriodsWithdrawn += uint64(periodsPast);
             }
             emit EarningsWithdrawn(requestReferences[i]);
         }
@@ -280,6 +324,7 @@ contract PinningManager {
 
     function _setStoragePrice(StorageOffer storage offer, uint64 period, uint64 price) internal {
         require(offer.maximumDuration >= period); //TODO: maybe we can remove this, if there is no attack vector.
+        period.mul(price); // overflow check. TODO: is this really needed?
         offer.prices[period] = price;
         emit PriceSet(msg.sender, period, price);
     }
