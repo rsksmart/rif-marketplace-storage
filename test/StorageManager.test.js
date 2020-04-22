50d11fd1f6b76c52151be52941de567694ec870f
Adam Uhlíř <adam@uhlir.dev>
2020-04-22 19:18:27 +0200

    feat: agreement funds support and operations around it

--- test/StorageManager.test.js
+++ test/StorageManager.test.js
@@ -1,69 +1,307 @@
-/* eslint-disable */
-const { expect } = require('chai');
+/* eslint-disable @typescript-eslint/no-var-requires,no-undef */
 const {
-  BN,
-  balance,
-  time,
   expectEvent,
-  expectRevert,
-} = require('@openzeppelin/test-helpers');
+  expectRevert
+} = require('@openzeppelin/test-helpers')
+const { asciiToHex, padRight, soliditySha3 } = require('web3-utils')
+const StorageManager = artifacts.require('StorageManager')
 
-const StorageManager = artifacts.require('StorageManager');
+function sleep (ms, ...args) {
+  return new Promise(resolve => setTimeout(() => resolve(...args), ms))
+}
 
-contract('StorageManager', ([PinningProvider, user, randomPerson]) => {
+function getAgreementReference (receipt) {
+  const newAgreementEvent = receipt.logs.find(e => e.event === 'NewAgreement')
+  return newAgreementEvent.args.agreementReference
+}
+
+contract('StorageManager', ([Provider, Consumer, randomPerson]) => {
+  let storageManager
+  const cid = [asciiToHex('/ipfs/QmSomeHash')]
+
   beforeEach(async function () {
-    this.storageManager = await StorageManager.new({ from: randomPerson });
-  });
+    storageManager = await StorageManager.new({ from: randomPerson })
+  })
 
-  describe('Create pinning offer', () => {
-    context('one price/duration point', () => {});
-    context('two price/duration points', () => {});
-    context('zero price/duration points', () => {
-      // revert
-    });
-    context('more than 256 price/duration points', () => {
-      // revert
-    });
-  });
+  describe('setOffer', () => {
+    it('should create new Offer for valid inputs', async () => {
+      const msg = [padRight(asciiToHex('some string'), 64), padRight(asciiToHex('some other string'), 64)]
+      const receipt = await storageManager.setOffer(1000, [10, 100], [10, 80], msg, { from: Provider })
+      expectEvent(receipt, 'AvailableCapacitySet', {
+        provider: Provider,
+        capacity: '1000'
+      })
+      expectEvent(receipt, 'BillingPlanSet', {
+        provider: Provider,
+        period: '10',
+        price: '10'
+      })
+      expectEvent(receipt, 'BillingPlanSet', {
+        provider: Provider,
+        period: '100',
+        price: '80'
+      })
 
-  describe('Update pinning offer (capacity)', () => {});
+      // TODO: Waiting for support of asserting arrays to be released for validation of emitted message.
+      expectEvent(receipt, 'MessageEmitted')
+    })
 
-  describe('Update pinning offer (price)', () => {});
+    it('should revert for no billing plan', async () => {
+      await expectRevert(storageManager.setOffer(1000, [], [], [], { from: Provider }), 'StorageManager: Offer needs some billing plans')
+    })
 
-  describe('Accept pinning offer', () => {
-    context('non-existant', () => {});
-    context('expired', () => {
-      context('final payout done', () => {});
-      context('final payout not done', () => {
-        // do final payment
-      });
-    });
-    context('non-expired', () => {
-      // revert
-    });
-  });
+    it('should revert when billing plans array is not the same size as billing prices', async () => {
+      await expectRevert(storageManager.setOffer(1000, [1], [1, 2], [], { from: Provider }), 'StorageManager: Billing plans array length has to equal to billing prices')
+    })
+  })
 
-  describe('Prolong contract', () => {
-    context('non-expired', () => {});
-    context('non-existant', () => {
-      // revert
-    });
-    context('expired', () => {
-      // revert
-    });
-    context('offer expired', () => {
-      // revert
-    });
-  });
+  describe('newAgreement', () => {
+    it('should create new Agreement for valid inputs', async () => {
+      await storageManager.setOffer(1000, [10, 100], [10, 80], [], { from: Provider })
 
-  describe('Request payment', () => {
-    context('expired', () => {});
-    context('not expired', () => {
-      context('linear function', () => {
-        context('half of price duration', () => {});
-        context('at price duration', () => {});
-        context('two times price duration', () => {});
-      });
-    });
-  });
-});
+      const receipt = await storageManager.newAgreement(cid, Provider, 100, 10, { from: Consumer, value: 2000 })
+      expectEvent(receipt, 'NewAgreement', {
+        provider: Provider,
+        agreementAuthor: Consumer,
+        size: '100',
+        billingPeriod: '10',
+        billingPrice: '10',
+        availableFunds: '2000'
+      })
+    })
+
+    it('should revert for no billing period or size', async () => {
+      await expectRevert(storageManager.newAgreement(cid, Provider, 0, 10, { from: Consumer, value: 2000 }),
+        'StorageManager: size has to be bigger then 0')
+      await expectRevert(storageManager.newAgreement(cid, Provider, 100, 0, { from: Consumer, value: 2000 }),
+        'StorageManager: billing period of 0 not allowed')
+    })
+
+    it('should revert when agreement already exists', async () => {
+      await storageManager.setOffer(1000, [10, 100], [10, 80], [], { from: Provider })
+      await storageManager.newAgreement(cid, Provider, 100, 10, { from: Consumer, value: 2000 })
+
+      await expectRevert(storageManager.newAgreement(cid, Provider, 100, 100, { from: Consumer, value: 2000 }),
+        'StorageManager: Agreement already active')
+    })
+
+    it('should revert for non existing Offer', async () => {
+      await expectRevert(storageManager.newAgreement(cid, Provider, 100, 10, { from: Consumer, value: 2000 }),
+        'StorageManager: Offer for this provider doesn\'t exist')
+    })
+
+    it('should revert for non existing Billing plan', async () => {
+      await storageManager.setOffer(1000, [10, 100], [10, 80], [], { from: Provider })
+      await expectRevert(storageManager.newAgreement(cid, Provider, 100, 20, { from: Consumer, value: 2000 }),
+        'StorageManager: billing price doesn\'t exist for Offer')
+    })
+
+    it('should revert when not enough value is deposited', async () => {
+      await storageManager.setOffer(1000, [10, 100], [10, 80], [], { from: Provider })
+      await expectRevert(storageManager.newAgreement(cid, Provider, 100, 10, { from: Consumer, value: 10 }),
+        'StorageManager: funds deposited has to be for at least one billing period')
+    })
+
+    it('should recreate expired Agreement ', async () => {
+      await storageManager.setOffer(1000, [1, 2], [10, 20], [], { from: Provider })
+      const agreementReference = getAgreementReference(await storageManager.newAgreement(cid, Provider, 100, 1, { from: Consumer, value: 1500 }))
+      await sleep(1000)
+
+      await storageManager.payoutFunds([agreementReference], { from: Provider })
+      const receipt = await storageManager.newAgreement(cid, Provider, 100, 2, { from: Consumer, value: 2000 })
+      expectEvent(receipt, 'NewAgreement', {
+        provider: Provider,
+        agreementAuthor: Consumer,
+        size: '100',
+        billingPeriod: '2',
+        billingPrice: '20',
+        availableFunds: '2500'
+      })
+    })
+  })
+
+  describe('depositFunds', function () {
+    it('should deposit funds for valid inputs', async () => {
+      await storageManager.setOffer(1000, [10, 100], [10, 80], [], { from: Provider })
+      await storageManager.newAgreement(cid, Provider, 100, 10, { from: Consumer, value: 2000 })
+
+      const receipt = await storageManager.depositFunds(cid, Provider, { from: Consumer, value: 100 })
+      expectEvent(receipt, 'AgreementFundsDeposited', {
+        amount: '100'
+      })
+    })
+
+    it('should revert when offer does not exists', async () => {
+      await expectRevert(storageManager.depositFunds(cid, Provider, { from: Consumer, value: 100 }),
+        'StorageManager: Offer for this Provider doesn\'t exist')
+    })
+
+    it('should revert when agreement does not exists', async () => {
+      await storageManager.setOffer(1000, [10, 100], [10, 80], [], { from: Provider })
+
+      await expectRevert(storageManager.depositFunds(cid, Provider, { from: Consumer, value: 100 }),
+        'StorageManager: Agreement for this Offer doesn\'t exist')
+    })
+
+    it('should revert when billing plans does not exist anymore', async () => {
+      await storageManager.setOffer(1000, [10, 100], [10, 80], [], { from: Provider })
+      await storageManager.newAgreement(cid, Provider, 100, 10, { from: Consumer, value: 2000 })
+      await storageManager.setBillingPlans([10, 100], [0, 80], { from: Provider })
+
+      await expectRevert(storageManager.depositFunds(cid, Provider, { from: Consumer, value: 100 }),
+        'StorageManager: price not available anymore')
+    })
+
+    it('should revert when agreement is expired', async () => {
+      await storageManager.setOffer(1000, [1, 100], [10, 80], [], { from: Provider })
+      const agreementReference = getAgreementReference(await storageManager.newAgreement(cid, Provider, 100, 1, { from: Consumer, value: 1500 }))
+      await sleep(1000)
+
+      await storageManager.payoutFunds([agreementReference], { from: Provider })
+      await expectRevert(storageManager.depositFunds(cid, Provider, { from: Consumer, value: 100 }),
+        'StorageManager: Agreement not active')
+    })
+  })
+
+  describe('withdrawFunds', function () {
+    it('should withdraw funds for valid inputs', async () => {
+      await storageManager.setOffer(1000, [10, 100], [10, 80], [], { from: Provider })
+      await storageManager.newAgreement(cid, Provider, 100, 10, { from: Consumer, value: 2000 })
+
+      const receipt = await storageManager.withdrawFunds(cid, Provider, 1000, { from: Consumer })
+      expectEvent(receipt, 'AgreementFundsWithdrawn', {
+        amount: '1000'
+      })
+    })
+
+    it('should withdraw all available funds when zero is passed', async () => {
+      await storageManager.setOffer(1000, [10, 100], [10, 80], [], { from: Provider })
+      await storageManager.newAgreement(cid, Provider, 100, 10, { from: Consumer, value: 2000 })
+
+      const receipt = await storageManager.withdrawFunds(cid, Provider, 0, { from: Consumer })
+      expectEvent(receipt, 'AgreementFundsWithdrawn', {
+        amount: '1000'
+      })
+    })
+
+    it('should revert when too big amount is requested to be withdrawn', async () => {
+      await storageManager.setOffer(1000, [10, 100], [10, 80], [], { from: Provider })
+      await storageManager.newAgreement(cid, Provider, 100, 10, { from: Consumer, value: 3000 })
+
+      // Should be 1 more ten limit. 100 (size) * 10 (price) = 1000 reserved and 2000 is available
+      await expectRevert(storageManager.withdrawFunds(cid, Provider, 2001, { from: Consumer }),
+        'StorageManager: Amount is too big')
+    })
+
+    it('should revert when somebody else then author of Agreement is trying to withdraw funds', async () => {
+      await storageManager.setOffer(1000, [10, 100], [10, 80], [], { from: Provider })
+      await storageManager.newAgreement(cid, Provider, 100, 10, { from: Consumer, value: 3000 })
+
+      await expectRevert(storageManager.withdrawFunds(cid, Provider, 1000, { from: randomPerson }),
+        "StorageManager: Agreement for this Offer doesn't exist")
+    })
+
+    it('should revert when amount exceeds current period', async () => {
+      await storageManager.setOffer(1000, [10, 100], [10, 80], [], { from: Provider })
+      await storageManager.newAgreement(cid, Provider, 100, 10, { from: Consumer, value: 2000 })
+
+      await expectRevert(storageManager.withdrawFunds(cid, Provider, 1001, { from: Consumer }),
+        'StorageManager: Amount is too big')
+    })
+
+    /**
+     * The agreements has to block funds for all previous periods that were still not-payed out
+     * to the Provider.
+     */
+    it('should revert when amount exceeds non-payed out periods', async () => {
+      await storageManager.setOffer(1000, [1, 10], [10, 80], [], { from: Provider })
+      await storageManager.newAgreement(cid, Provider, 100, 1, { from: Consumer, value: 5000 })
+      await sleep(1000)
+
+      await expectRevert(storageManager.withdrawFunds(cid, Provider, 3001, { from: Consumer }),
+        'StorageManager: Amount is too big')
+    })
+
+    it('should withdraw all funds except reserved funds for past periods and current period', async () => {
+      await storageManager.setOffer(1000, [1, 100], [10, 80], [], { from: Provider })
+      await storageManager.newAgreement(cid, Provider, 100, 1, { from: Consumer, value: 5000 })
+      await sleep(1000)
+
+      const receipt = await storageManager.withdrawFunds(cid, Provider, 0, { from: Consumer })
+      expectEvent(receipt, 'AgreementFundsWithdrawn', {
+        amount: '3000'
+      })
+    })
+
+    it('should withdraw zero funds because everything is reserved', async () => {
+      await storageManager.setOffer(1000, [1, 100], [10, 80], [], { from: Provider })
+      await storageManager.newAgreement(cid, Provider, 100, 1, { from: Consumer, value: 5000 })
+      await sleep(4000)
+
+      const receipt = await storageManager.withdrawFunds(cid, Provider, 0, { from: Consumer })
+      expectEvent(receipt, 'AgreementFundsWithdrawn', {
+        amount: '0'
+      })
+    })
+
+    it('should revert when offer does not exists', async () => {
+      await expectRevert(storageManager.withdrawFunds(cid, Provider, 100, { from: Consumer }),
+        'StorageManager: Offer for this Provider doesn\'t exist')
+    })
+
+    it('should revert when agreement does not exists', async () => {
+      await storageManager.setOffer(1000, [10, 100], [10, 80], [], { from: Provider })
+
+      await expectRevert(storageManager.withdrawFunds(cid, Provider, 100, { from: Consumer }),
+        'StorageManager: Agreement for this Offer doesn\'t exist')
+    })
+
+    it('should withdraw all remaining funds when the agreement is expired', async () => {
+      await storageManager.setOffer(1000, [1, 100], [10, 80], [], { from: Provider })
+      const agreementReference = getAgreementReference(await storageManager.newAgreement(cid, Provider, 100, 1, { from: Consumer, value: 2500 }))
+      await sleep(2100)
+
+      const payoutReceipt = await storageManager.payoutFunds([agreementReference], { from: Provider })
+      expectEvent(payoutReceipt, 'AgreementFundsPayout', {
+        agreementReference,
+        amount: '2000'
+      })
+      expectEvent(payoutReceipt, 'AgreementStopped', {
+        agreementReference
+      })
+
+      const withdrawReceipt = await storageManager.withdrawFunds(cid, Provider, 0, { from: Consumer })
+      expectEvent(withdrawReceipt, 'AgreementFundsWithdrawn', {
+        amount: '500'
+      })
+    })
+  })
+
+  describe('payoutFunds', function () {
+    it('should payout funds for valid inputs', async () => {
+      await storageManager.setOffer(1000, [1, 100], [10, 80], [], { from: Provider })
+      const agreementReference = getAgreementReference(await storageManager.newAgreement(cid, Provider, 100, 1, { from: Consumer, value: 5000 }))
+      await sleep(1900)
+
+      const receipt = await storageManager.payoutFunds([agreementReference], { from: Provider })
+      expectEvent(receipt, 'AgreementFundsPayout', {
+        amount: '2000'
+      })
+    })
+
+    it('should payout funds and stop agreement when run out of funds', async () => {
+      await storageManager.setOffer(1000, [1, 100], [10, 80], [], { from: Provider })
+      const agreementReference = getAgreementReference(await storageManager.newAgreement(cid, Provider, 100, 1, { from: Consumer, value: 2500 }))
+      await sleep(2100)
+
+      const receipt = await storageManager.payoutFunds([agreementReference], { from: Provider })
+      expectEvent(receipt, 'AgreementFundsPayout', {
+        agreementReference,
+        amount: '2000'
+      })
+      expectEvent(receipt, 'AgreementStopped', {
+        agreementReference
+      })
+    })
+  })
+})
