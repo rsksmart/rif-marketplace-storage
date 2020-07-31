9d1f52b3bae87ac05a9e7e757ecb8391fc767476
Vojtech Simetka <vojtech@simetka.cz>
2020-07-31 11:13:51 +0200

    chore: added config for ganache

--- truffle-config.js
+++ truffle-config.js
@@ -38,6 +38,11 @@ module.exports = {
    */
 
   networks: {
+    ganache: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 8545,            // Standard Ethereum port (default: none)
+     network_id: "*",       // Any network (default: none)
+    },
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
     // You should run a client (like ganache-cli, geth or parity) in a separate terminal
