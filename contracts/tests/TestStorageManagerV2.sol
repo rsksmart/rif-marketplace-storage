59d5101f22c58c514e8239babdbd31da1c4b7320
Alvaro Fariña <farinalvaro@gmail.com>
2021-07-21 16:05:50 -0300

    fix(tests): keeps time variable when upgrading storage manager contract

--- contracts/tests/TestStorageManagerV2.sol
+++ contracts/tests/TestStorageManagerV2.sol
@@ -4,6 +4,20 @@ pragma experimental ABIEncoderV2;
 import "../StorageManager.sol";
 
 contract TestStorageManagerV2 is StorageManager {
+    uint256 time;
+
+    function _time() internal view override returns (uint256) {
+        return time;
+    }
+
+    function incrementTime(uint256 increment) external {
+        time = time + increment;
+    }
+
+    function setTime(uint256 newTime) external {
+        time = newTime;
+    }
+
     function getVersion() external pure returns (string memory) {
         return "V2";
     }
